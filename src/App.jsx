32d989ea63d778513a8ff1134976f3bd0745c63e
Deepak Tyagi <tyagideepak2004@gmail.com>
2026-02-20 11:16:39 +0530

    Update portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
